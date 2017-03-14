7f472b82ff964ee537cc0dfb1980314c92aabde3
g572-528r <845828099@qq.com>
2017-03-14 11:04:23 +0800

    baidu_map_地图

---
+++ app/src/main/java/com/example/g572_528r/as0313_map/LocationClient.java
@@ -0,0 +1,7 @@
+package com.example.g572_528r.as0313_map;
+
+/**
+ * Created by g572-528r on 2017/3/14.
+ */
+public class LocationClient {
+}
---
+++ app/src/main/java/com/example/g572_528r/as0313_map/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.g572_528r.as0313_map;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.TextureMapView;
+
+public class MainActivity extends AppCompatActivity {
+    TextureMapView mTextureMapView;
+    private TextView tvName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // 申请一个（或多个）权限，并提供用于回调返回的获取码（用户定义）
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1001);
+        }
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        tvName = (TextView) findViewById(R.id.tv_name);
+        mTextureMapView = (TextureMapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mTextureMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mTextureMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mTextureMapView.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            // requestCode即所声明的权限获取码，在checkSelfPermission时传入
+            case 1001 :
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 获取到权限，作相应处理（调用定位SDK应当确保相关权限均被授权，否则可能引起定位失败）
+                } else {
+                    // 没有获取到权限，做特殊处理
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
